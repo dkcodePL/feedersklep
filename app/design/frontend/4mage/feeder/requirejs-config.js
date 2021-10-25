c8794a8d1199001a4a4ec8de91b4d0e80691b28b
Dev Feeder <dev@feedersklep.pl>
2021-10-25 12:17:04 +0200

    slick file path

--- app/design/frontend/4mage/feeder/requirejs-config.js
+++ app/design/frontend/4mage/feeder/requirejs-config.js
@@ -2,7 +2,7 @@ var config = {
     map: {
         '*': {
             'vanilla-lazyLoad': 'js/lib/lazyload.amd.min',
-            'slick': 'js/lib/slick.min',
+            'slick': 'js/lib/slick'
             'collapsibleBlock': 'js/widget/collapsible-block',
             'slickCarousel': 'js/widget/slick-carousel'
         }
